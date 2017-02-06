b1f25b6b5d85d9698d379853d54530082694e1e2
jschluger <jschluger@stuy.edu>
2017-02-06 13:45:45 -0500

    adding

---
+++ db_reader.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+import csv
+
+#connect to server on lisa
+server = MongoClient('lisa.stuy.edu')
+#server = MongoClient()
+
+#open (or create) db
+ourDB = server['SecondTerm']
+
+students = ourDB.students.find()
+
+for student in students:
+    
+    print student
+    
