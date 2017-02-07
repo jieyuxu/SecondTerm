85cedf87c06fa38b1407b68b1723d6af5796ba4d
jieyuxu <jieyu.stuy@gmail.com>
2017-02-07 12:33:41 -0500

    updated db builder for teachers.csv

--- db_builder.py
+++ db_builder.py
@@ -9,21 +9,29 @@ server = MongoClient('lisa.stuy.edu')
 ourDB = server['SecondTerm']                                                                                                                                                                                
                                                                                                                                                                                                             
 fObj = open("peeps.csv")                                                                                                                                                                                    
-d = csv.DictReader(fObj) #students dict                                                                                                                                                                     
+d = csv.DictReader(fObj) #students dict   
+
+tObj = open("teachers.csv")                                                                                                                                                                                    
+t = csv.DictReader(tObj) #teachers dict                                                                                                                                                                      
+                                          
+for teacher in t:
+    teacher['students'] = []
                                                                                                                                                                                                             
-                                                                                                                                                                                                            
-for student in d:                                                                                                                                                                                           
-    student['courses'] = []                                                                                                                                                                                 
-    gObj = open("courses.csv")                                                                                                                                                                              
-    g = csv.DictReader(gObj)                                                                                                                                                                                
-                                                                                                                                                                                                            
-    #print student.keys()                                                                                                                                                                                   
-                                                                                                                                                                                                            
-    for course in g:                                                                                                                                                                                        
-                                                                                                                                                                                                            
-        #print course                                                                                                                                                                                       
-        if student['id'] == course['id']:                                                                                                                                                                   
-            student['courses'].append({ course['code'] : course['mark']})                                                                                                                                   
-                                                                                                                                                                                                            
-    print student                                                                                                                                                                                           
-    ourDB.students.insert_one( student )     
+    for student in d:
+        student['courses'] = []
+        gObj = open("courses.csv")
+        g = csv.DictReader(gObj) 
+                   
+        #print student.keys() 
+                                                                                                                                                                                                                
+        for course in g:
+                       
+            #print course                                                                                                                                                                                       
+            if student['id'] == course['id']:
+                student['courses'].append({ course['code'] : course['mark']})
+                if course['code'] == teacher['code']:
+                    teacher['students'].append(student['name'])
+                                             
+        print student
+        ourDB.students.insert_one( student )
+    ourDB.teachers.insert_one( teacher )
