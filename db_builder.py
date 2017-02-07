9ebce761baeb09dd65d0b69bf9042f1129d6ff50
jieyuxu <jieyu.stuy@gmail.com>
2017-02-06 22:04:58 -0500

    averages, printing of name, avg, id now works.

--- db_builder.py
+++ db_builder.py
@@ -1,26 +1,29 @@
-from pymongo import MongoClient
-import csv
-
-#connect to server on lisa
-server = MongoClient('lisa.stuy.edu')
-#server = MongoClient()
-
-#open (or create) db
-ourDB = server['SecondTerm']
-
-fObj = open("peeps.csv") 
-d = csv.DictReader(fObj) #students dict
-
-for student in d:
-    student['courses'] = []   
-    #print "student: ", student
-    
-    #you have to re-open this before ever itteration
-    gObj = open("courses.csv")
-    g = csv.DictReader(gObj) #course dict
-    for course in g:
-        #print "course: ", course
-        if student['id'] == course['id']:
-            student['courses'].append( { course['code']: course['mark'] } )
-    #print "adding this student doc to db: ", student
-    ourDB.students.insert_one( student )
+from pymongo import MongoClient                                                                                                                                                                             
+import csv                                                                                                                                                                                                  
+                                                                                                                                                                                                            
+#connect to server on lisa                                                                                                                                                                                  
+server = MongoClient('lisa.stuy.edu')                                                                                                                                                                       
+#server = MongoClient()                                                                                                                                                                                     
+                                                                                                                                                                                                            
+#open (or create) db                                                                                                                                                                                        
+ourDB = server['SecondTerm']                                                                                                                                                                                
+                                                                                                                                                                                                            
+fObj = open("peeps.csv")                                                                                                                                                                                    
+d = csv.DictReader(fObj) #students dict                                                                                                                                                                     
+                                                                                                                                                                                                            
+                                                                                                                                                                                                            
+for student in d:                                                                                                                                                                                           
+    student['courses'] = []                                                                                                                                                                                 
+    gObj = open("courses.csv")                                                                                                                                                                              
+    g = csv.DictReader(gObj)                                                                                                                                                                                
+                                                                                                                                                                                                            
+    #print student.keys()                                                                                                                                                                                   
+                                                                                                                                                                                                            
+    for course in g:                                                                                                                                                                                        
+                                                                                                                                                                                                            
+        #print course                                                                                                                                                                                       
+        if student['id'] == course['id']:                                                                                                                                                                   
+            student['courses'].append({ course['code'] : course['mark']})                                                                                                                                   
+                                                                                                                                                                                                            
+    print student                                                                                                                                                                                           
+    ourDB.students.insert_one( student )     
--- db_reader.py
+++ db_reader.py
@@ -1,16 +1,25 @@
-from pymongo import MongoClient
-import csv
-
-#connect to server on lisa
-server = MongoClient('homer.stuy.edu')
-#server = MongoClient()
-
-#open (or create) db
-ourDB = server['SecondTerm']
-
-students = ourDB.students.find()
-
-for student in students:
-    
-    print student
-    
+from pymongo import MongoClient                                                                                                                                                                             
+import csv                                                                                                                                                                                                  
+                                                                                                                                                                                                            
+#connect to server on lisa                                                                                                                                                                                  
+server = MongoClient('lisa.stuy.edu')                                                                                                                                                                       
+#server = MongoClient()                                                                                                                                                                                     
+                                                                                                                                                                                                            
+#open (or create) db                                                                                                                                                                                        
+ourDB = server['SecondTerm']                                                                                                                                                                                
+                                                                                                                                                                                                            
+students = ourDB.students.find()                                                                                                                                                                            
+                                                                                                                                                                                                            
+def compAvg(d):                                                                                                                                                                                             
+    total = 0.00                                                                                                                                                                                            
+                                                                                                                                                                                                            
+    for dic in d:                                                                                                                                                                                           
+        for key in dic:                                                                                                                                                                                     
+            total += int(dic[key])                                                                                                                                                                          
+                                                                                                                                                                                                            
+    return total / len(d)                                                                                                                                                                                   
+                                                                                                                                                                                                            
+for student in students:                                                                                                                                                                                    
+    #print compAvg(student["courses"])                                                                                                                                                                      
+    #print student["name"]                                                                                                                                                                                  
+    print "name: %s, age: %s, average: %f" % (student["name"], student["id"], compAvg(student["courses"]))     
