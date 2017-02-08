b55fc0433248d2b192a30b2c6c96894d4ec4ba4c
jschluger <jschluger@stuy.edu>
2017-02-08 13:51:28 -0500

    removing more silly spaces

--- db_builder.py
+++ db_builder.py
@@ -1,31 +1,28 @@
-from pymongo import MongoClient                                                                                                                                                                             
-import csv                                                                                                                                                                                                  
-                                                                                                                                                                                                             
- #connect to server on lisa                                                                                                                                                                                  
-server = MongoClient('lisa.stuy.edu')                                                                                                                                                                       
-#server = MongoClient()                                                                                                                                                                                     
-                                                                                                                                                                                                             
-#open (or create) db                                                                                                                                                                                        
-ourDB = server['SecondTerm']                                                                                                                                                                                
-                                                                                                                                                                                                             
-fObj = open("peeps.csv")                                                                                                                                                                                    
+from pymongo import MongoClient
+import csv
+#connect to server on lisa
+server = MongoClient('lisa.stuy.edu')
+#server = MongoClient()
+#open (or create) db
+ourDB = server['SecondTerm']
+fObj = open("peeps.csv")
+
 d = csv.DictReader(fObj) #students dict
 
 tObj = open("teachers.csv")
-t = csv.DictReader(tObj)                                                                                                                                                                                                
-                                                                                                                                                                                                             
-for student in d:                                                                                                                                                                                           
-    student['courses'] = []                                                                                                                                                                                 
-    gObj = open("courses.csv")                                                                                                                                                                              
-    g = csv.DictReader(gObj)                                                                                                                                                                                
-                                                                                                                                                                                                             
-    #print student.keys()                                                                                                                                                                                   
-                                                                                                                                                                                                             
-    for course in g:                                                                                                                                                                                        
-                                                                                                                                                                                                             
-         #print course                                                                                                                                                                                       
-        if student['id'] == course['id']:                                                                                                                                                                   
-             student['courses'].append({ course['code'] : course['mark']})                                                                                                                                   
-                                                                                                                                                                                                             
-    #print student                                                                                                                                                                                           
+t = csv.DictReader(tObj)
+
+for student in d:
+    student['courses'] = []
+
+    gObj = open("courses.csv")
+    g = csv.DictReader(gObj)
+
+    #print student.keys()
+    for course in g:
+        #print course
+        if student['id'] == course['id']:
+            student['courses'].append({ course['code'] : course['mark']})
+            #print student
+
     ourDB.students.insert_one( student )      
